e171f9f32b02f1000818ebb23b006dd44f26dc51
52eraja <122664422+52eraja@users.noreply.github.com>
2023-07-10 16:38:44 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,7 +117,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          B布鲁斯AI
+          布鲁斯AI
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own ChatGPT.
